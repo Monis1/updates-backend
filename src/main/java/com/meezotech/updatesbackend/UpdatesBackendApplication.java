0f8687d87d6978a6366160dd8d89d283901e5d26
Monis Ahmed <monis.ahmed@venturedive.com>
2018-01-21 01:10:11 +0500

    Initial commit

---
+++ src/main/java/com/meezotech/updatesbackend/UpdatesBackendApplication.java
@@ -0,0 +1,12 @@
+package com.meezotech.updatesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdatesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpdatesBackendApplication.class, args);
+	}
+}
