1ca278b533e4d032aa646bbe1ad354c6c6247478
Monis Ahmed <monis.ahmed@venturedive.com>
2018-02-04 01:26:26 +0500

    Enabled Security Again

--- src/main/java/com/meezotech/updatesbackend/security/config/WebSecurityConfig.java
+++ src/main/java/com/meezotech/updatesbackend/security/config/WebSecurityConfig.java
@@ -1,46 +1,46 @@
-//package com.meezotech.updatesbackend.security.config;
-//
-//import com.meezotech.updatesbackend.security.filter.JWTAuthenticationFilter;
-//import com.meezotech.updatesbackend.security.filter.JWTLoginFilter;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//
-///**
-// * The type Web security configuration.
-// * Class added for JWTs, copied from https://auth0.com/blog/implementing-jwt-authentication-on-spring-boot/
-// */
-//@Configuration
-//@EnableWebSecurity
-//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.csrf().disable().authorizeRequests()
-//                .antMatchers("/").permitAll()
-//                .antMatchers(HttpMethod.POST, "/token").permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                // We filter the api/login requests
-//                .addFilterBefore(new JWTLoginFilter("/token", authenticationManager()),
-//                        UsernamePasswordAuthenticationFilter.class)
-//                // And filter other requests to check the presence of JWT in header
-//                .addFilterBefore(new JWTAuthenticationFilter(),
-//                        UsernamePasswordAuthenticationFilter.class)
-//        .logout();
-//    }
-//
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        // Create a default account
-//        auth.inMemoryAuthentication()
-//                .withUser("meezotech")
-//                .password("mangomanmango")
-//                .roles("ADMIN");
-//    }
-//
-//}
+package com.meezotech.updatesbackend.security.config;
+
+import com.meezotech.updatesbackend.security.filter.JWTAuthenticationFilter;
+import com.meezotech.updatesbackend.security.filter.JWTLoginFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * The type Web security configuration.
+ * Class added for JWTs, copied from https://auth0.com/blog/implementing-jwt-authentication-on-spring-boot/
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers(HttpMethod.POST, "/token").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                // We filter the api/login requests
+                .addFilterBefore(new JWTLoginFilter("/token", authenticationManager()),
+                        UsernamePasswordAuthenticationFilter.class)
+                // And filter other requests to check the presence of JWT in header
+                .addFilterBefore(new JWTAuthenticationFilter(),
+                        UsernamePasswordAuthenticationFilter.class)
+        .logout();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // Create a default account
+        auth.inMemoryAuthentication()
+                .withUser("meezotech")
+                .password("mangomanmango")
+                .roles("ADMIN");
+    }
+
+}
